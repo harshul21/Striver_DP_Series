154493804f4fb92b1c0d3f1caa8974165df6c0a4
harshul <harshulgarg21@gmail.com>
2022-07-12 13:22:25 +0530

    Code Updated

--- Ninja’s Training.cpp
+++ Ninja’s Training.cpp
@@ -55,3 +55,27 @@ int ninjaTraining(int n, vector<vector<int>> &points)
     return dp[n-1][3];
 }
 
+//Space Optimisation
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<int> prev(4,0);
+    
+    prev[0]=max(points[0][1],points[0][2]);
+    prev[1]=max(points[0][0],points[0][2]);
+    prev[2]=max(points[0][0],points[0][1]);
+    prev[3]=max(points[0][0],max(points[0][1],points[0][2]));
+    
+    for(int day=1;day<n;day++){
+        vector<int> tmp(4,0);
+        for(int last=0;last<4;last++){
+            tmp[last]=0;
+            for(int task=0;task<3;task++){
+                if(task!=last){
+                    tmp[last]=max(tmp[last],points[day][task]+prev[task]);
+                }
+            }
+        }
+        prev=tmp;
+    }
+    return prev[3];
+}
