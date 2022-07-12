a1dfad8b7fb2b3cebb624954a1a28ca0c82d6a1b
harshul <harshulgarg21@gmail.com>
2022-07-12 08:21:44 +0530

    Code Added

---
+++ Maximum sum of non-adjacent elements .cpp
@@ -0,0 +1,27 @@
+//Youtube Link:-https://www.youtube.com/watch?v=GrMBfJNk_NY&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=6
+//Problem Link:- https://www.codingninjas.com/codestudio/problems/maximum-sum-of-non-adjacent-elements_843261?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+
+//Memoisation or Top Down
+#include<bits/stdc++.h>
+
+int solveUtil(int ind, vector<int>& arr, vector<int>& dp){
+    
+    if(dp[ind]!=-1) return dp[ind];
+    
+    if(ind==0) return arr[ind];
+    if(ind<0)  return 0;
+    
+    int pick= arr[ind]+ solveUtil(ind-2, arr,dp);
+    int nonPick = 0 + solveUtil(ind-1, arr, dp);
+    
+    return dp[ind]=max(pick, nonPick);
+}
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n=nums.size();
+    vector<int> dp(n,-1);
+    return solveUtil(n-1,nums,dp);
+}
+
+//Bottom Up
+
