d578baee848383b81a7a1b0006b505c7fbb04513
harshul <harshulgarg21@gmail.com>
2022-07-12 08:31:25 +0530

    Code updated

--- Maximum sum of non-adjacent elements .cpp
+++ Maximum sum of non-adjacent elements .cpp
@@ -23,5 +23,23 @@ int maximumNonAdjacentSum(vector<int> &nums){
     return solveUtil(n-1,nums,dp);
 }
 
-//Bottom Up
+//Space Optimisation
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    int n=nums.size();
+    int prev=nums[0];
+    int prev2=0;
+    for(int i=1;i<n;i++){
+        int take=nums[i];
+        if(i>1){
+            take+=prev2;
+        }
+        
+        int notTake=0+prev;
+        int curr=max(take,notTake);
+        prev2=prev;
+        prev=curr;
+    }
+    return prev;
+}
 
