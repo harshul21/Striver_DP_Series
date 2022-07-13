18a81a1eca301862d221824f90206278b70ec160
harshul <harshulgarg21@gmail.com>
2022-07-13 09:55:35 +0530

    Code Updated

--- Triangle.cpp
+++ Triangle.cpp
@@ -48,3 +48,18 @@ int minimumPathSum(vector<vector<int>>& triangle, int n){
 }
 
 //Space Optimisation
+int minimumPathSum(vector<vector<int>>& triangle, int n){
+    vector<int> front(n,0),curr(n,0);
+    for(int j=0;j<n;j++){
+        front[j]=triangle[n-1][j];
+    }
+    for(int i=n-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+            int d=triangle[i][j]+front[j];
+            int dg=triangle[i][j]+front[j+1];
+            curr[j]=min(d,dg);
+        }
+        front=curr;
+    }
+    return front[0];
+}
