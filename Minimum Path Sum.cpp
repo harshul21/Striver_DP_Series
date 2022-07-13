cae25a2d9f4042a69aa82086c5314a29ecdced29
harshul <harshulgarg21@gmail.com>
2022-07-13 08:52:50 +0530

    Code updated

--- Minimum Path Sum.cpp
+++ Minimum Path Sum.cpp
@@ -57,3 +57,38 @@ int minSumPath(vector<vector<int>> &grid) {
     }
     return dp[n-1][m-1];
 }
+
+//Space Optimisation
+int minSumPath(vector<vector<int>> &grid) {
+    int n=grid.size();
+    int m=grid[0].size();
+    vector<int> prev(m,0);
+    for(int i=0;i<n;i++){
+        vector<int> curr(m,0);
+        for(int j=0;j<m;j++){
+            if(i==0 and j==0){
+                curr[i]=grid[i][j];
+            }
+            else{
+                int up=grid[i][j];
+                if(i>0){
+                    up+=prev[j];
+                }
+                else{
+                    up+=1e9;
+                }
+                
+                int left=grid[i][j];
+                if(j>0){
+                    left+=curr[j-1];
+                }
+                else{
+                    left+=1e9;
+                }
+                curr[i]=min(up,left);
+            }
+        }
+        prev=curr;
+    }
+    return prev[m-1];
+}
