a5ef995b14e009ffe769e82168d966fd64b67021
harshul <harshulgarg21@gmail.com>
2022-07-14 11:39:35 +0530

    DP on subsequence

--- Partition Equal Subset Sum.cpp
+++ Partition Equal Subset Sum.cpp
@@ -35,3 +35,86 @@ bool canPartition(vector<int> &arr, int n)
     return dfs(arr,n-1,sum,dp);
 }
 
+//Top Down
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        
+        if(sum%2==1){
+            return false;
+        }
+        
+        sum=sum/2;
+        vector<vector<bool>> dp(n,vector<bool>(sum+1,false));
+        
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        
+        if(nums[0]<=sum){
+            dp[0][nums[0]]=true;
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=sum;j++){
+                bool ntake=dp[i-1][j];
+                
+                bool take=false;
+                if(nums[i]<=j){
+                    take=dp[i-1][j-nums[i]];
+                }
+                
+                dp[i][j]= ntake || take;
+            }
+        }
+        return dp[n-1][sum];
+    }
+};
+
+//Space Optimisation of tabulation method
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        
+        if(sum%2==1){
+            return false;
+        }
+        
+        sum=sum/2;
+        vector<int> prev(sum+1,false);
+        
+        
+        prev[0]=true;
+        
+        if(nums[0]<=sum){
+            prev[nums[0]]=true;
+        }
+        
+        for(int i=1;i<n;i++){
+            vector<int> curr(sum+1,false);
+            curr[0]=true;
+            for(int j=1;j<=sum;j++){
+                bool ntake=prev[j];
+                
+                bool take=false;
+                if(nums[i]<=j){
+                    take=prev[j-nums[i]];
+                }
+                
+                curr[j]= ntake || take;
+            }
+            prev=curr;
+        }
+        return prev[sum];
+    }
+};
