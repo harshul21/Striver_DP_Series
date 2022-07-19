17760940e44b8f5c2b5a1f0e1fe49ac5e99de97c
harshul <harshulgarg21@gmail.com>
2022-07-19 14:43:50 +0530

    DP on LIS

---
+++ Number of Longest Increasing Subsequence.cpp
@@ -0,0 +1,30 @@
+//Youtube Link:- https://www.youtube.com/watch?v=cKVl1TFdNXg&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=48
+//Problem Link:- https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,1), cnt(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[prev]<nums[i] and 1+dp[prev]>dp[i]){
+                    dp[i]=1+dp[prev];
+                    cnt[i]=cnt[prev];
+                }
+                else if(nums[prev]<nums[i] and 1+dp[prev]==dp[i]){
+                    cnt[i]+=cnt[prev];
+                }
+            }
+            maxi=max(maxi,dp[i]);
+        }
+        int nos=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==maxi){
+                nos+=cnt[i];
+            }
+        }
+        return nos;
+    }
+};
