d45cc6066bab38941805d1494b207b4cd3f59088
harshul <harshulgarg21@gmail.com>
2022-07-23 23:43:09 +0530

    DP on Partition(MCM)

--- Cost to Cut a Chocolate.cpp
+++ Cost to Cut a Chocolate.cpp
@@ -25,3 +25,41 @@ int cost(int n, int c, vector<int> &cuts){
     vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
     return dfs(1,c,cuts,dp);
 }
+
+//Top Down
+#include<bits/stdc++.h>
+int dfs(int i,int j,vector<int> &cuts,vector<vector<int>> &dp){
+    if(i>j)
+        return 0;
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    int mini=INT_MAX;
+    for(int idx=i;idx<=j;idx++){
+        int cost=cuts[j+1]-cuts[i-1]+dfs(i,idx-1,cuts,dp)+dfs(idx+1,j,cuts,dp);
+        
+        mini=min(mini,cost);
+    }
+    return dp[i][j]=mini;
+}
+
+int cost(int n, int c, vector<int> &cuts){
+    cuts.push_back(n);
+    cuts.insert(cuts.begin(),0);
+    sort(cuts.begin(),cuts.end());
+    vector<vector<int>> dp(c+2,vector<int>(c+2,0));
+    for(int i=c;i>=1;i--){
+        for(int j=1;j<=c;j++){
+            if(i>j)
+                continue;
+            int mini=INT_MAX;
+            for(int idx=i;idx<=j;idx++){
+                int cost=cuts[j+1]-cuts[i-1]+dp[i][idx-1]+dp[idx+1][j];
+
+                mini=min(mini,cost);
+            }
+            dp[i][j]=mini;
+        }
+    }
+    return dp[1][c];
+}
